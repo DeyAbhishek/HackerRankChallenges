140ec1d02c9dd8bbdcde4b87c82a352a38a35c63
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-02-07 19:22:58 -0500

    Create Pairs.cpp

---
+++ Pairs/Pairs.cpp
@@ -0,0 +1,54 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <cstdlib>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+/* Head ends here */
+
+int pairs(vector < int > a,int k) {
+   
+    
+    int ans = 0;
+    std:sort(a.begin(), a.end());
+    vector<int>::iterator p = a.begin();
+    while(p!=a.end()){
+        int a1 = *p + k;
+        vector<int>::iterator itr = find(a.begin(), a.end(), a1);
+        if(itr!=a.end()){ans++;}
+        p++;
+    }
+    return ans;
+}
+
+/* Tail starts here */
+int main() {
+    int res;
+    
+    int _a_size,_k;
+    cin >> _a_size>>_k;
+    cin.ignore (std::numeric_limits<std::streamsize>::max(), '\n'); 
+    vector<int> _a;
+    int _a_item;
+    for(int _a_i=0; _a_i<_a_size; _a_i++) {
+        cin >> _a_item;
+        _a.push_back(_a_item);
+    }
+    
+    res = pairs(_a,_k);
+    cout << res;
+    
+    return 0;
+}
