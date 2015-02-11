cc9bbbe505eb757b4bfc6ba3892b855e3645e953
Abhishek Dey <DeyAbhishek@users.noreply.github.com>
2015-02-11 18:32:54 -0500

    Update maximumSubarray.cpp

--- The-MaximumSubArrayContiguous-NonContiguous/maximumSubarray.cpp
+++ The-MaximumSubArrayContiguous-NonContiguous/maximumSubarray.cpp
@@ -23,6 +23,18 @@ int main() {
             sum += arr[i];
             if(sum < 0) sum = 0;
             if(max < sum) max = sum;
+            
+        }
+        if(max < 0) max = 0;
+        int count = 0;
+        int maxElem = arr[0];
+        for(i = 0; i < n; i++){
+            if(arr[i] < 0) count++;
+            if(arr[i] > maxElem) maxElem = arr[i];
+        }
+        if(count == n) {
+            noncontisum = maxElem;
+            max = maxElem;
         }
         cout << max << " " << noncontisum << endl;
     }
